da3f0fc4e263b36bd8fece1ec92308cc2ccbaf8f
Saurav <saurav24021998@gmail.com>
2025-06-01 13:04:52 +0530

    feat(campaign): add CRUD apis for campaign management - added AddCampaignApi, DeleteCampaignApi, UpdateCampaignApi - Implemented CampaignController - Modified AddressController for consistency

---
+++ src/main/java/com/bannrx/common_service/controller/CampaignController.java
@@ -0,0 +1,39 @@
+package com.bannrx.common_service.controller;
+
+import com.bannrx.common.dtos.campaign.CampaignDto;
+import com.bannrx.common_service.apis.AddCampaignApi;
+import com.bannrx.common_service.apis.DeleteCampaignApi;
+import com.bannrx.common_service.apis.UpdateCampaignApi;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import rklab.utility.annotations.Loggable;
+import rklab.utility.dto.ApiOutput;
+import rklab.utility.expectations.InvalidInputException;
+import rklab.utility.expectations.ServerException;
+
+
+@Loggable
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("v1/api/campaign")
+public class CampaignController {
+    private final AddCampaignApi addCampaignApi;
+    private final UpdateCampaignApi updateCampaignApi;
+    private final DeleteCampaignApi deleteCampaignApi;
+
+
+    @PostMapping("/register")
+    public ApiOutput<?> register(@RequestBody CampaignDto dto) throws ServerException, InvalidInputException {
+        return addCampaignApi.process(dto);
+    }
+
+    @PutMapping("/update")
+    public ApiOutput<?> update(@RequestBody CampaignDto dto) throws InvalidInputException, ServerException {
+        return updateCampaignApi.update(dto);
+    }
+
+    @DeleteMapping("/delete")
+    public ApiOutput<?> delete(@RequestParam String campaignId) throws InvalidInputException {
+        return deleteCampaignApi.process(campaignId);
+    }
+}
