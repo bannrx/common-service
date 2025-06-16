67cb039a6a6cb63f6a22c89da0abd2ce62bb0928
ritik-agrawal <ritikagrawal1697@gmail.com>
2025-06-16 16:36:14 +0530

    version update

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.bannrx"
-version = "common-service-0.0.6-SNAPSHOT"
+version = "common-service-0.0.7-SNAPSHOT"
 
 java {
 	toolchain {
@@ -71,7 +71,7 @@ dependencies {
 		}
 		implementation(project(":utility"))
 	} else {
-		implementation("com.bannrx:bannrx-common:common-0.0.7-SNAPSHOT"){
+		implementation("com.bannrx:bannrx-common:common-0.0.8-SNAPSHOT"){
 			exclude(group="com.rklab", module="utility")
 		}
 		implementation("com.rklab:utility:utility-0.0.5-SNAPSHOT")
